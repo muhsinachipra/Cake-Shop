13d92c3fa5621e7ae6b47e29dfc7414db180e063
muhsinachipra <muhsinachipra@gmail.com>
2024-03-27 20:56:22 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const redux = require('redux');
+const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
+
+const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+
+
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        payload: 1
+    }
+}
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload,
+            }
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
+console.log('Intial state ', store.getState())
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Update state ', store.getState())
+})
+
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockCake(3))
+
+const action = bindActionCreators({ orderCake, restockCake }, store.dispatch)
+action.orderCake()
+action.orderCake()
+action.orderCake()
+action.restockCake(3)
+
+unsubscribe()
